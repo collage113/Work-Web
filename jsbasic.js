8ab1015b12723ac1963e9b8b6a2f768c7dec8e3e
collage113 <55772461+collage113@users.noreply.github.com>
2019-10-01 16:21:09 +0900

    Add files via upload

---
+++ jsbasic.js
@@ -0,0 +1,97 @@
+//jsbasic.js
+document.write("<h3>JavaScript 기본 언어 스펙 연습<h3>");
+document.write("<hr>"); 
+
+var box;
+box = 100;
+document.write(box, "<br>");
+console.log(box);
+document.write("<br>");
+
+//문자형
+var s ="JavaScript";
+var num = 100;
+var tag = "<h3>JavaScript String</h3>";
+
+document.write(s, "<br>" );
+document.write(num, "<br>" );
+document.write(tag, "<br>");
+document.write("<br>");
+
+//숫자형
+var n =100;
+var t = Number( '500');
+
+document.write(n, "<br>");
+document.write(t, "<br>");
+document.write("<br>");
+
+// 논리형
+var b = true;
+var b2 = 10>=100;
+var k = Boolean("hello");
+var l = Boolean(0);
+var m = Boolean(null);
+
+document.write(b, "<br>");
+document.write(b2, "<br>");
+document.write(k, "<br>");
+document.write(l, "<br>");
+document.write(m, "<br>");
+document.write("<br>");
+
+//null & undifined
+var u;
+var h = "hello";
+h = null;
+
+document.write(u, "<br>");
+document.write(h, "<br>");
+document.write("<br>");
+
+// typeof
+document.write(typeof s, "<br>");
+document.write(typeof n, "<br>");
+document.write(typeof b, "<br>");
+document.write(typeof u, "<br>");
+document.write(typeof h, "<br>");
+document.write("<br>");
+
+
+//문자열 결합 연산
+var str = "<table border = '1'>";
+str += "<tr>";
+str += "<td>1</td><td>/2<td></td>3</td>";
+str += "</tr>";
+str += "<table>";
+
+document.write(str, "<br>");
+document.write("<br>");
+
+//비교 연산 값만 같으면 자료형은 t/f를 구별하지 않는다.
+var k =10, m ="10";
+
+document.write(k==10, "<br>");
+document.write(m==10, "<br>");
+document.write("<br>");
+
+
+document.write(k==10, "<br>");
+document.write(m==10, "<br>");
+document.write("<br>");
+
+//입력
+var number1 = prompt("정수1 입력", 0);
+var number2 = prompt("정수2 입력", 0);
+var sum = parseInt(number1)+parseInt(number2);
+
+document.write(number1 +" + "+number2+ " = " +sum, "<br>");
+document.write("<br>");
+
+var select = confirm("회원을 탈퇴시키겠습니까?");
+
+if(select) {
+    document.write("탈퇴 처리 되었습니다.");
+} else {
+    document.write("탈퇴 취소되었습니다.");
+}
